b6b41716fe436d29c4d346a1ca2bef675195a0fa
Tarocafe <55334852+Tarocafe@users.noreply.github.com>
2022-06-04 00:28:11 +0200

    inital commit

---
+++ res/js/preview-size.js
@@ -0,0 +1,20 @@
+const preview = $("#designer .preview")
+
+function refreshPreviewSize() {
+    let wh = window.innerHeight
+    let ww = window.innerWidth
+    let vlw = $("#designer .views-list").offsetWidth 
+    let tph = $(".top-panel").offsetHeight
+
+    preview.style.transform = `translateX(${vlw}px) translateY(${tph}px) scale(${(ww - vlw) / ww}) `
+    preview.style.borderBottom = `solid ${ww / (ww - vlw)}px var(--border-color)`
+    preview.style.opacity = "1"
+}
+
+refreshPreviewSize()
+
+window.onresize = () => refreshPreviewSize()
+
+preview.oncontextmenu = event => {
+    event.preventDefault();
+}
---
+++ res/js/taroutils.js
@@ -0,0 +1,5 @@
+const $ = selector => document.querySelector(selector)
+const $$ = selector => document.querySelectorAll(selector)
+const log = str => console.log(str)
+const body = $("body")
+const html = $("html")
